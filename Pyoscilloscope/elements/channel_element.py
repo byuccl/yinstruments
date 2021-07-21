77155b8f43842a8cb670c1eb0e419cf55f821fc4
spriteman1200 <spriteman1200@gmail.com>
2021-07-21 16:23:55 -0600

    changes with wave data

--- Pyoscilloscope/elements/channel_element.py
+++ Pyoscilloscope/elements/channel_element.py
@@ -14,6 +14,12 @@ class Channel_Element:
         }
 
     def __getitem__(self, key):
+        command_format = self.channel_format + str(key) + self.command + self.command_query
+        self.__instr.write(command_format)
+        scope_value = self.__instr.read()
+        if scope_value != self.channel[key]:
+            print("Warning:", self.command, "on channel", str(key), "is set to", self.channel[key], "but the scope is set to", scope_value, ". Changing to scope value")
+            self.channel[key] = scope_value
         return self.channel[key]
 
     def __setitem__(self, key, value):
@@ -70,8 +76,28 @@ class Attenuation(Channel_Element):
 class Wave_Premable(Channel_Element):
     pass
 
-class Wave_Data(Channel_Element):
-    pass
+class Wave_Data():
+    def __init__(self, instr, channel_format, command, command_query):
+        self.__instr = instr
+        self.channel_format = channel_format
+        self.command = command
+        self.command_query = command_query
+        self.channel = {
+            1: None,
+            2: None,
+            3: None,
+            4: None
+        }
+
+    def __getitem__(self, key):
+        
+        return self.channel[key]
+
+    def __setitem__(self, key, value):
+        self.channel[key] = value
+        if self.channel[key]:
+            command_format = self.channel_format + str(key) + self.command + " " + str(self.channel[key])
+            self.__instr.write(command_format)
     
 
 
