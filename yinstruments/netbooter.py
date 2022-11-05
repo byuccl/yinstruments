f2bc80e5ad1d2bb60f5632e33e2fcc9cc82b08c8
Jeff Goeders <jeff.goeders@gmail.com>
2022-11-05 12:23:56 -0600

    Reorg netbooter
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- yinstruments/netbooter.py
+++ yinstruments/netbooter.py
@@ -1,63 +1,67 @@
+#!/usr/bin/python3
+
 import telnetlib
 import time
+import sys
+import os
 import re
 
 
-class Netbooter:
-    def __init__(self, ip, port, timeout=3.0):
-        self.ip = ip
+SLEEP_TIME = 1
+
+TIMEOUT = 3.0
+
+
+class Interface:
+    def __init__(self, ip_address, port):
+        self.ip_address = ip_address
         self.port = port
-        self._SLEEP_TIME = 1
-        self.timeout = timeout
 
     def port_reboot(self, port_num):
-        tn = telnetlib.Telnet(self.ip, self.port, timeout=self.timeout)
+        tn = telnetlib.Telnet(self.ip_address, self.port, timeout=TIMEOUT)
 
         s = tn.read_some()
-        time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
 
         s = ("rb " + str(port_num)).encode("ascii") + b"\r\n\r\n"
         tn.write(s)
-        time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
         tn.close()
 
-    def port_on(self, port_num, do_sleep=True):
+    def port_on(self, port_num):
         # print("On:", port_num)
-        tn = telnetlib.Telnet(self.ip, self.port, timeout=self.timeout)
+        tn = telnetlib.Telnet(self.ip_address, self.port, timeout=TIMEOUT)
 
         s = tn.read_some()
-        time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
 
         s = ("pset " + str(port_num) + " 1").encode("ascii") + b"\r\n\r\n"
         tn.write(s)
-        if do_sleep:
-            time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
         tn.close()
 
-    def port_off(self, port_num, do_sleep=True):
+    def port_off(self, port_num):
         # print("Off:", port_num)
-        tn = telnetlib.Telnet(self.ip, self.port, timeout=self.timeout)
+        tn = telnetlib.Telnet(self.ip_address, self.port, timeout=TIMEOUT)
 
         s = tn.read_some()
         # print(s)
-        time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
 
         s = ("pset " + str(port_num) + " 0").encode("ascii") + b"\r\n\r\n"
         tn.write(s)
-        if do_sleep:
-            time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
         tn.close()
 
-    def get_status(self, do_sleep=True):
-        tn = telnetlib.Telnet(self.ip, self.port, timeout=self.timeout)
+    def get_status(self):
+        tn = telnetlib.Telnet(self.ip_address, self.port, timeout=TIMEOUT)
 
         s = tn.read_some()
-        time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
 
         s = "pshow".encode("ascii") + b"\r\n"
         tn.write(s)
-        if do_sleep:
-            time.sleep(self._SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
 
         s = ""
         while True:
@@ -79,3 +83,43 @@ class Netbooter:
             if m:
                 return m.group(1) == "ON"
         return None
+
+    def print_usage():
+        print("Incorrect usage!")
+        print(os.path.splitext(__file__)[0], "<on|off|reboot|status> <port_num>")
+
+
+def main():
+    ip_address = "192.168.1.100"
+    port = 23
+    netbooter = Interface(ip_address, port)
+    # Get command
+    if len(sys.argv) < 2:
+        netbooter.print_usage()
+        return
+
+    cmd = sys.argv[1].lower()
+    if cmd == "status":
+        print(netbooter.get_status())
+        return
+
+    if len(sys.argv) != 3:
+        netbooter.print_usage()
+        return
+
+    if cmd not in ("on", "off", "reboot"):
+        netbooter.print_usage()
+        return
+
+    port_num = sys.argv[2]
+
+    if cmd == "on":
+        netbooter.port_on(port_num)
+    elif cmd == "off":
+        netbooter.port_off(port_num)
+    elif cmd == "reboot":
+        netbooter.port_reboot(port_num)
+
+
+if __name__ == "__main__":
+    main()
