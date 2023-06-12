20a9d3ec64b55abd5ae1ba29761f116151083233
Jeff Goeders <jeff.goeders@gmail.com>
2023-06-12 12:46:52 -0600

    fix formatting
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- test/test_packaging.py
+++ test/test_packaging.py
@@ -3,8 +3,8 @@ import unittest
 from yinstruments.pdu.lindy import Lindy
 from yinstruments.pdu.netbooter import Netbooter
 
-class TestPackaging(unittest.TestCase):
 
+class TestPackaging(unittest.TestCase):
     def test_pdus(self):
         lindy = Lindy("192.168.0.0", "80")
         netbooter = Netbooter("192.168.0.0", "80")
