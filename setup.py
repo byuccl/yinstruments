35eae8e0896f51e4ed5b952152a4c47d50225049
Jeff Goeders <jeff.goeders@gmail.com>
2024-11-20 11:45:09 -0700

    Fix async uart bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="yinstruments",
     packages=find_packages(),
-    version="1.6.1",
+    version="1.6.2",
     description="Experiment device control scripts for BYU's Configurable Computing Lab (https://ccl.byu.edu/)",
     author="Jeff Goeders",
     author_email="jeff.goeders@gmail.com",
